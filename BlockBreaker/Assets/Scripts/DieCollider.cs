d2ef93f6dec971630f43f0c836eabb679f476dd7
Sungin Jung <jungs@uchicago.edu>
2017-10-17 15:57:20 -0500

    finish physics

---
+++ BlockBreaker/Assets/Scripts/DieCollider.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DieCollider : MonoBehaviour {
+	public LevelManager levelManaber;
+
+	void OnTriggerEnter2D(Collider2D collision) {
+		print ("Trigger");
+		levelManaber.LoadLevel ("Win");
+	}
+
+}
---
+++ BlockBreaker/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public Text start;
+
+	public void LoadLevel(string level){
+
+		Debug.Log ("Clicked!" + level );
+		SceneManager.LoadScene (level);
+	}
+}
